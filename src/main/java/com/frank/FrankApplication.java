addb323bbe1aa3820ffa46a7ac79af0605d3d515
zhiangrcc <zhiangrcc@gmail.com>
2019-07-21 13:13:41 +0800

    first commit

---
+++ src/main/java/com/frank/FrankApplication.java
@@ -0,0 +1,14 @@
+package com.frank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class FrankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrankApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/frank/FrankApplicationTests.java
@@ -0,0 +1,16 @@
+package com.frank;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FrankApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
